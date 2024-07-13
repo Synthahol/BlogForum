ba1f4ef1e07525bd0c58c2537196fe27ad525402
Synthahol <74485907+Synthahol@users.noreply.github.com>
2024-07-13 15:24:47 +0900

    database?

--- app.py
+++ app.py
@@ -104,8 +104,9 @@ if not app.debug:
     app.logger.info("Blog startup")
 
 
-def create_tables():
+with app.app_context():
     db.create_all()
+
 
 @login_manager.user_loader
 def load_user(user_id):
