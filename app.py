3f3e50e6bd80c5a00bc7d07143cfb896dc2563cc
Synthahol <74485907+Synthahol@users.noreply.github.com>
2024-07-15 19:38:17 +0900

    Laast one for the day i f****** swear

--- app.py
+++ app.py
@@ -579,5 +579,15 @@ def react():
         return jsonify(status="error", message=form.errors)
 
 
+@app.after_request
+def add_header(response):
+    response.headers["Cache-Control"] = (
+        "no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0"
+    )
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "-1"
+    return response
+
+
 if __name__ == "__main__":
     app.run(debug=False)
